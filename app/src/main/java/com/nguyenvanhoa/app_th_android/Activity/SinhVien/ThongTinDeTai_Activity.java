8990bd1f7fc3a4d2ef5470a6585b3b8cf7a8c3ca
hoava <hoavannguyen682001@gmail.com>
2021-12-16 20:50:23 +0700

    upload đủ thứ

---
+++ app/src/main/java/com/nguyenvanhoa/app_th_android/Activity/SinhVien/ThongTinDeTai_Activity.java
@@ -0,0 +1,16 @@
+package com.nguyenvanhoa.app_th_android.Activity.SinhVien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nguyenvanhoa.app_th_android.R;
+
+public class ThongTinDeTai_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thong_tin_de_tai);
+    }
+}
